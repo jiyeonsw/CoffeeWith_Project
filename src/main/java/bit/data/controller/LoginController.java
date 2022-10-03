e90f5ce94c092733b44b71fb8d5ceb31dc94465d
keentrynjoy <ijkim24@naver.com>
2022-10-04 03:06:20 +0900

    [Feat,Design] 김성민 - MyPage 1. 'tiles' 사용하여 레이아웃 지정 2. 북마크, 리뷰 데이터 추가 후 개수(count) 출력 완료. 3. 로그인이 아직 안된 관계로 임시로 세션 주기/세션 제거 기능 구현

--- src/main/java/bit/data/controller/LoginController.java
+++ src/main/java/bit/data/controller/LoginController.java
@@ -48,21 +48,33 @@ public class LoginController {
         session.removeAttribute("login_nick");
     }
 
-    //임시 세션 저장
+    //[임시] 세션 저장
     @GetMapping("/call_session")
     @ResponseBody
     public void callSession(HttpSession session) {
         session.setMaxInactiveInterval(60 * 60 * 12);
-        
+
         UserDto dto = userService.selectDataById(2);
 
-        System.out.println(dto.getUserId());
-        System.out.println(dto.getEmailId());
-        System.out.println(dto.getUserNick());
+//        System.out.println(dto.getUr_id());
+//        System.out.println(dto.getEmail_id());
+//        System.out.println(dto.getUr_nk());
 
         session.setAttribute("login_ok", "yes");
-        session.setAttribute("login_id", dto.getEmailId());
-        session.setAttribute("login_nick", dto.getUserNick());
+        session.setAttribute("login_id", dto.getUr_id());
+        session.setAttribute("login_nick", dto.getUr_nk());
+    }
+
+    //[임시] 세션 저장
+    @GetMapping("/del_session")
+    @ResponseBody
+    public void delSession(HttpSession session) {
+
+        session.removeAttribute("login_ok");
+        session.removeAttribute("login_id");
+        session.removeAttribute("login_nick");
+        session.removeAttribute("cfCmtCnt");
+        session.removeAttribute("cfLkCnt'");
     }
 }
 
--- src/main/java/bit/data/service/UserServiceInter.java
+++ src/main/java/bit/data/service/UserServiceInter.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public interface UserServiceInter {
 
-    public int selectSearchId(String emailId);
+    public int selectSearchId(String email_id);
 
-    public int selectSearchNick(String userNick);
+    public int selectSearchNick(String ur_nk);
 
     public void insertUser(UserDto dto);
 
     public List<String> selectSubstrSi();
 
-    public UserDto selectDataById(int userId);
+    public UserDto selectDataById(int ur_id);
 
 	/*public int getTotalCount();
 	public List<UserDto> getAllusers();
