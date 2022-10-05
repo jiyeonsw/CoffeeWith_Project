28f96f1d6025154832ec7ef41da6e04825bb18ac
keentrynjoy <ijkim24@naver.com>
2022-10-05 16:52:45 +0900

    [Feat] 김성민 10-05 16:44 MyPage 북마크 기능 구현중 - 북마크 카페리스트 사진을 제외한 정보 출력 완료.

--- src/main/java/bit/data/service/MypageService.java
+++ src/main/java/bit/data/service/MypageService.java
@@ -1,15 +1,18 @@
 package bit.data.service;
 
 import bit.data.dao.MypageDao;
+import bit.data.dto.CafeDto;
+import bit.data.dto.CafeImgDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MypageService implements MypageServiceInter {
 
     @Autowired
     MypageDao mypageDao;
-
 
     @Override
     public int selectCfLkCnt(int loginId) {
@@ -20,4 +23,15 @@ public class MypageService implements MypageServiceInter {
     public int selectCfCmtCnt(int loginId) {
         return mypageDao.selectCfCmtCnt(loginId);
     }
+
+    @Override
+    public List<CafeDto> selectCfIdNm(int loginId) {
+        return mypageDao.selectCfIdNm(loginId);
+    }
+
+
+    public List<CafeImgDto> selectCfLkCi(int cfId) {
+        return mypageDao.selectCfLkCi(cfId);
+    }
+
 }
