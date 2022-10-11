4adb15a34ab19daa5f66cbdd3b23006a66c44ec3
keentrynjoy <ijkim246@naver.com>
2022-10-11 13:48:59 +0900

    ComTourDto 에러 수정 후 서버 정상 작동 확인. 김성민

---
+++ src/main/java/bit/data/dto/ComTourDto.java
@@ -0,0 +1,36 @@
+package bit.data.dto;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class ComTourDto {
+
+    private int tr_id;
+    private int ur_id;
+    private String tr_nm;
+    private String tr_txt;
+    private String tr_pw;
+    private String tr_loc;
+    private int tw_max;
+    private String s_date;
+    private String e_date;
+    private Timestamp w_date;
+    private Timestamp u_date;
+    private String ur_nk;
+    private String ur_nm;
+    private String ur_pw;
+    private String ur_txt;
+    private String ur_img;
+    private String email_id;
+    private String naver_id;
+    private String kakao_id;
+    private String google_id;
+    private String loc_si;
+    private String loc_gu;
+    private String status;
+    private int report;
+    private String ur_phnum;
+}
+
