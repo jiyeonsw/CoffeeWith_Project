d2a5359cbb57ca2a5fe472117d8afea71ebdfbfe
keentrynjoy <ijkim246@naver.com>
2022-10-12 17:01:38 +0900

    [Feat,Bug,Design] 10-12 5:00 PM - MyPage 김성민 1.회원정보관리: 프로필사진 수정 안했을때 적용안되는 버그 수정 2.북마크 페이징 기능 구현 3.북마크 리스트 CSS 정리 4.header CSS 수정

--- src/main/java/bit/data/controller/MypageController.java
+++ src/main/java/bit/data/controller/MypageController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,20 +54,19 @@ public class MypageController {
     @GetMapping("/bookmarks")
     public String bookmarkList(
             @RequestParam(defaultValue = "1") int currentPage,
-            @RequestParam(value = "searchColumn", required = false) String sc,
-            @RequestParam(value = "searchWord", required = false) String sw,
             HttpSession session, Model model) {
         int loginId = (int) session.getAttribute("login_id");
 
         //paging 처리
         int totalCount = mypageService.selectCfLkCnt(loginId);
         int perPage = 12;// 한페이지당 보여질 북마크의 개수
-        int perBlock = 5;// 한블럭당 보여질 페이지의 개수
+        int perBlock = 3;// 한블럭당 보여질 페이지의 개수
         int startNum; // db에서 가져올 북마크 시작번호(mysql 0부터 시작)
         int startPage;// 각 블럭당 보여질 시작페이지
         int endPage;// 각 블럭당 보여질 끝페이지
         int totalPage;// 총 페이지수
         int no;// 각 페이지당 출력할 시작번호
+        int testNum;
         // 총 페이지수를 구하기
         // 총 북마크의 개수/한페이지당 보여질 개수로 나눔(7/5=1)
         // 나머지가 1이라도 있으면 무조건 1 페이지 추가(1+1=2페이지가 필요)
@@ -76,29 +76,43 @@ public class MypageController {
         // perBlock=5 일경우 현재페이지가 1~5 일경우는 시작페이지가 1, 끝페이지가 5
         // 만약 현재페이지가 13 일경우는 시작페이지가 11, 끝페이지가 15
         startPage = (currentPage - 1) / perBlock * perBlock + 1;
-        System.out.println("startPage : " + startPage);
         endPage = startPage + perBlock - 1;
-        System.out.println("endPage : " + endPage);
+
         // 총페이지수가 23개일경우 마지막 블럭은 끝페이지가 25가 아니라 23이라야한다
         if (endPage > totalPage)
             endPage = totalPage;
 
         // 각 페이지에서 보여질 시작번호
-        // 예: 1페이지->0, 2페이지:5, 3페이지 : 10...
+        // 예: 1페이지->0, 2페이지:12, 3페이지 : 24...
         startNum = (currentPage - 1) * perPage;
-        System.out.println("startNum : " + startNum);
+
 
         // 각페이지당 출력할 시작번호 구하기
         // 예: 총글갯수가 23이라면 1페이지는 23,2페이지는 18,3페이지는 13...
         no = totalCount - (currentPage - 1) * perPage;
-        System.out.println("no : " + no);
+
+        testNum = startNum + perPage;
+
+//        System.out.println("startPage : " + startPage);
+//        System.out.println("endPage : " + endPage);
+//        System.out.println("startNum : " + startNum);
+//        System.out.println("no : " + no);
+//        System.out.println("testNum" + testNum);
 
         List<MyPageCafeLikeDto> list = mypageService.selectLikeCfInfo(loginId);
-//        for (int i = 0; i < perPage; i++) {
-//            MyPageCafeLikeDto sliceDto = list.get(i);
-//            List<MyPageCafeLikeDto> slist = new ArrayList<>();
-//            slist.add(sliceDto);
-//        }
+        List<MyPageCafeLikeDto> slist = new ArrayList<>();
+
+        for (int i = startNum; i < testNum; i++) {
+            try {
+                if (list.get(i).getCf_nm() != null) {
+                    MyPageCafeLikeDto sliceDto = list.get(i);
+//                System.out.println(sliceDto.getCf_nm());
+                    slist.add(sliceDto);
+                }
+            } catch (IndexOutOfBoundsException iobe) {
+                iobe.getMessage();
+            }
+        }
 
         model.addAttribute("totalCount", totalCount);
         model.addAttribute("currentPage", currentPage);
@@ -106,7 +120,7 @@ public class MypageController {
         model.addAttribute("endPage", endPage);
         model.addAttribute("no", no);
         model.addAttribute("totalPage", totalPage);
-        model.addAttribute("list", list);
+        model.addAttribute("list", slist);
 
         int cfLkCnt = mypageService.selectCfLkCnt(loginId);
         int cfCmtCnt = mypageService.selectCfCmtCnt(loginId);
